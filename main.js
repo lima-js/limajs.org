416922221c608d1476114763aeb110d3f37c3e22
Moisés Cachay <xpktro@gmail.com>
2019-03-02 12:27:21 -0500

    Usa fetch para extraer las imágenes de las redes sociales y traer el código del archivo principal al futuro

--- main.js
+++ main.js
@@ -1,30 +1,14 @@
 'use strict';
 
-var request = function (url, cb) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.open('get', url, true);
-  xhr.onload = function () {
-    if (xhr.status !== 200) {
-      return cb && cb(xhr.status);
-    }
-    cb && cb(null, xhr.response);
-  };
-
-  xhr.send();
-};
-
-var social = function (container) {
-  var handleRequestCallback = function (child, err, svg) {
-    child.innerHTML = (svg || '');
-  };
-
-  [].forEach.call(container.children, function (child) {
-    request('img/' + child.className + '.svg', handleRequestCallback.bind(null, child));
+const social = (container) => {
+  [].forEach.call(container.children, (child) => {
+    fetch('img/' + child.className + '.svg')
+      .then(response => response.text())
+      .then(svg => { child.innerHTML = svg });
   });
 };
 
-var main = function () {
+const main = () => {
   social(document.getElementsByClassName('social')[0]);
 };
 
