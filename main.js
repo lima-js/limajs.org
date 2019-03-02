7b31848e2d76c137faba8d5a4ac32886b018955e
Moisés Cachay <xpktro@gmail.com>
2019-03-02 11:55:56 -0500

    Refacciones de estilo al javascript principal

--- main.js
+++ main.js
@@ -1,44 +1,32 @@
 'use strict';
 
-
 var request = function (url, cb) {
-
   var xhr = new XMLHttpRequest();
 
   xhr.open('get', url, true);
-
   xhr.onload = function () {
-
     if (xhr.status !== 200) {
       return cb && cb(xhr.status);
     }
-
     cb && cb(null, xhr.response);
   };
 
   xhr.send();
 };
 
-
 var social = function (container) {
-
   var handleRequestCallback = function (child, err, svg) {
-
     child.innerHTML = (svg || '');
   };
 
   [].forEach.call(container.children, function (child) {
-
     request('img/' + child.className + '.svg', handleRequestCallback.bind(null, child));
   });
 };
 
-
-var main = function (container) {
-
+var main = function () {
   social(document.getElementsByClassName('social')[0]);
 };
-
 
 window.addEventListener('load', main);
 
