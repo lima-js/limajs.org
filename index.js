6456ef7c54aee7394b07233051c7b35735ae076e
wixo <wxomb@Juans-MacBook.local>
2019-04-16 18:42:10 -0500

    Prettier pass

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 'use strict';
 
-
-var request = function (url, cb) {
-
+var request = function(url, cb) {
   var xhr = new XMLHttpRequest();
 
   xhr.open('get', url, true);
 
-  xhr.onload = function () {
-
+  xhr.onload = function() {
     if (xhr.status !== 200) {
       return cb && cb(xhr.status);
     }
@@ -19,25 +16,18 @@ var request = function (url, cb) {
   xhr.send();
 };
 
-
-var social = function (container) {
-
-  var handleRequestCallback = function (child, err, svg) {
-
-    child.innerHTML = (svg || '');
+var social = function(container) {
+  var handleRequestCallback = function(child, err, svg) {
+    child.innerHTML = svg || '';
   };
 
-  [].forEach.call(container.children, function (child) {
-
+  [].forEach.call(container.children, function(child) {
     request('images/icons/' + child.className + '.svg', handleRequestCallback.bind(null, child));
   });
 };
 
-
-var main = function (container) {
-
+var main = function(container) {
   social(document.getElementsByClassName('social')[0]);
 };
-
 
 window.addEventListener('load', main);
