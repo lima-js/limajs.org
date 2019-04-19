7563ece2aaa283de22b647359f107974abd429d3
wixo <wxomb@Juans-MacBook.local>
2019-04-19 15:43:28 -0500

    Modernize social logic
    A lot has happened in 2 years.

--- index.js
+++ index.js
@@ -19,34 +19,18 @@ const colorLogo = () => {
   document.addEventListener('scroll', event => onWindowScroll({ event, logo, scheme }));
 };
 
-var request = function(url, cb) {
-  var xhr = new XMLHttpRequest();
-
-  xhr.open('get', url, true);
-
-  xhr.onload = function() {
-    if (xhr.status !== 200) {
-      return cb && cb(xhr.status);
-    }
-
-    cb && cb(null, xhr.response);
-  };
-
-  xhr.send();
-};
-
-var social = function(container) {
-  var handleRequestCallback = function(child, err, svg) {
-    child.innerHTML = svg || '';
-  };
-
-  [].forEach.call(container.children, function(child) {
-    request('images/icons/' + child.className + '.svg', handleRequestCallback.bind(null, child));
+const replaceSocialIcons = container => {
+  [...container.children].forEach(child => {
+    fetch('images/icons/' + child.className + '.svg')
+      .then(response => response.text())
+      .then(svg => {
+        child.innerHTML = svg;
+      });
   });
 };
 
-var main = function(container) {
-  social(document.getElementsByClassName('social')[0]);
+const main = () => {
+  replaceSocialIcons(document.querySelector('section.social'));
   colorLogo();
 };
 
