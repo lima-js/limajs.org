bcb320f0f032d05179283f389ed24d9f23ac86b4
Juan La Jara <wxomb@Juans-MacBook.local>
2019-06-05 00:20:41 -0500

    Prettier + Google analytics 😆

--- index.js
+++ index.js
@@ -107,10 +107,19 @@ window.addEventListener('load', main);
 //
 // Google analytics tracker
 //
-(function(l,i,m,a,_,j,s){l['GoogleAnalyticsObject']=_;l[_]=l[_]||function(){
-(l[_].q=l[_].q||[]).push(arguments)},l[_].l=1*new Date();j=i.createElement(m),
-s=i.getElementsByTagName(m)[0];j.async=1;j.src=a;s.parentNode.insertBefore(j,s)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+(function(l, i, m, a, _, j, s) {
+  l['GoogleAnalyticsObject'] = _;
+  (l[_] =
+    l[_] ||
+    function() {
+      (l[_].q = l[_].q || []).push(arguments);
+    }),
+    (l[_].l = 1 * new Date());
+  (j = i.createElement(m)), (s = i.getElementsByTagName(m)[0]);
+  j.async = 1;
+  j.src = a;
+  s.parentNode.insertBefore(j, s);
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-37026516-1', 'auto');
 ga('send', 'pageview');
