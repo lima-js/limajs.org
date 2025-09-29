97d83ae4df26b049665650449e4b5d999526afac
Lupo Montero <lupomontero@gmail.com>
2025-09-30 00:24:06 +0200

    Corrige sección de próximos eventos en homepage

--- index.js
+++ index.js
@@ -12,41 +12,6 @@ const replaceSocialIcons = container => {
   });
 };
 
-const fetchEvents = () =>
-  new Promise((resolve, reject) => {
-    const callbackId = `__callback__${Date.now()}`;
-    const script = document.createElement('script');
-    window[callbackId] = resolve;
-
-    script.src = `//api.meetup.com/LimaJS/events?callback=${callbackId}`;
-    script.onerror = reject;
-    document.body.appendChild(script);
-  });
-
-const buildRegistrationButton = (link = '#', container) => {
-  container.setAttribute('href', link);
-};
-
-const buildScheduleTitle = ({ time, venue: { lat, lon, name } }) => {
-  const container = $('[data-js="section-title"]');
-  const venue = container.querySelector('.venue');
-  const month = container.querySelector('.month');
-  const monthFromDate = new Date(time).toLocaleDateString('es', { month: 'long' });
-  const gMapsFromLatLong = `//maps.google.com/?q=${lat},${lon}`;
-
-  venue.innerText = name;
-  venue.setAttribute('href', gMapsFromLatLong);
-  month.innerText = monthFromDate;
-};
-
-const buildSchedule = container =>
-  fetchEvents()
-    .then(({ data: content }) => {
-      container.innerHTML += snarkdown(content[0].description);
-      return content[0];
-    })
-    .catch(console.error.bind(console));
-
 const buildSponsors = container => {
   fetch('./README.md')
     .then(response => response.text())
@@ -69,6 +34,19 @@ const buildSponsors = container => {
     });
 };
 
+const parseDuration = (duration) => {
+  const match = duration.match(/PT(?:(\d+)H)?(?:(\d+)M)?/);
+
+  if (!match) {
+    return 0;
+  }
+
+  const hours = parseInt(match[1] || '0', 10);
+  const minutes = parseInt(match[2] || '0', 10);
+
+  return (hours * 60 + minutes) * 60 * 1000;
+};
+
 const formatDateTimeToGCalendar = dateTime => {
   return dateTime
     .toISOString()
@@ -80,29 +58,83 @@ const formatDateTimeToGCalendar = dateTime => {
     .join('');
 };
 
-const addToCalendarLink = ({ container, content: { time, duration } }) => {
+const createAddToCalendarLink = (event) => {
   const baseURL = 'https://calendar.google.com/calendar/r/eventedit';
   const eventName = 'Lima JS';
-  const startDateTime = formatDateTimeToGCalendar(new Date(time));
-  const endDateTime = formatDateTimeToGCalendar(new Date(time + duration));
-  const link = `${baseURL}?text=${eventName}&dates=${startDateTime}/${endDateTime}`;
+  const dateTime = new Date(event.dateTime);
+  const startDateTime = formatDateTimeToGCalendar(dateTime);
+  const duration = parseDuration(event.duration);
+  const endDateTime = formatDateTimeToGCalendar(new Date(+dateTime + duration));
+  return `${baseURL}?text=${eventName}&dates=${startDateTime}/${endDateTime}`;
+};
 
-  container.setAttribute('href', link);
+const buildUpcomingEvents = (container) => {
+  return fetch('/data/upcoming-events.json')
+    .then(resp => resp.json())
+    .then((events) => {
+      if (!events.length) {
+        container.innerHTML = `
+          <p>
+            No hay eventos programados por el momento. Síguenos en nuestras
+            redes sociales para estar al tanto de las novedades.
+          </p>
+        `;
+        return;
+      }
+
+      container.innerHTML = '';
+
+      events.forEach((event) => {
+        const eventEl = document.createElement('div');
+        eventEl.classList.add('event');
+        eventEl.classList.add('panel');
+
+        const contentBox = document.createElement('div');
+        contentBox.classList.add('content-box');
+
+        contentBox.innerHTML = `
+          <h2>
+            <a href="${event.eventUrl}" target="_blank">${event.title}</a>
+          </h2>
+          <p>
+          <strong>Cuándo:</strong> ${
+            new Date(event.dateTime).toLocaleString('es-PE', {
+              timeZone: 'America/Lima',
+              dateStyle: 'full',
+              timeStyle: 'short',
+            })
+          }
+          </p>
+          <p><strong>Dónde:</strong> ${event.venues[0].name}</p>
+          ${snarkdown(event.description)}
+        `;
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        buttons.innerHTML = `
+          <a class="custom-button main" href="${event.eventUrl}">Registrarse</a>
+          <a
+            class="custom-button invert add-to-calendar"
+            href="${createAddToCalendarLink(event)}"
+          >
+            Agregar a Calendario
+          </a>
+        `;
+
+        eventEl.appendChild(contentBox);
+        eventEl.appendChild(buttons);
+        container.appendChild(eventEl);
+      });
+    })
+    .catch(console.error.bind(console));
 };
 
 const main = () => {
   replaceSocialIcons($('div.social'));
   replaceSocialIcons($('div.social-footer'));
-  buildSchedule($('div.LimaJS-schedule'))
-    .then(content => {
-      addToCalendarLink({ container: $('a.add-to-calendar'), content });
-      return content;
-    })
-    .then(content =>
-      buildRegistrationButton(content.link, $('[data-js="registration-button"]')) || content,
-    )
-    .then(content => buildScheduleTitle(content));
   buildSponsors($('div.sponsors'));
+  buildUpcomingEvents($('.events'));
 };
 
 window.addEventListener('load', main);
